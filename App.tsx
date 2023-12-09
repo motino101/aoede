001876f40cf61c6821161b23d14899b4ceb82bd1
cole <xintong@stanford.edu>
2023-12-09 10:10:53 -0800

    Inter Font Loaded

--- App.tsx
+++ App.tsx
@@ -5,22 +5,39 @@ import FormPage from './src/pages/FormPage';
 import ChatPage from './src/pages/ChatPage';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-import loadFonts from './src/utils/loadfonts'; // Adjust the path as necessary
+// import loadFonts from './src/utils/loadfonts'; // Adjust the path as necessary
 
+
+import { useFonts } from 'expo-font'
 
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  // const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  // useEffect(() => {
+  //   loadFonts().then(() => setFontsLoaded(true));
+  // }, []);
+
+  // if (!fontsLoaded) {
+  //   console.log('Fonts not loaded yet');
+  // }
 
   useEffect(() => {
-    loadFonts().then(() => setFontsLoaded(true));
-  }, []);
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded])
 
-  if (!fontsLoaded) {
-    console.log('Fonts not loaded yet');
+  if (!loaded) {
+    return null;
   }
+  
   console.log('Fonts loaded!');
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
